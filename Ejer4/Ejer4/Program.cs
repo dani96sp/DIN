36af55b73824935de7b8126203ff63487dc5faa7
dani96sp <d.dani96.d@gmail.com>
2019-10-17 03:41:30 +0200

    Update con los ejercicios de casa

---
+++ Ejer4/Ejer4/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string stNombre, stApellido;
+            stNombre = "Pepe";
+            stApellido = Console.ReadLine();
+
+            stNombre = stNombre + " " + stApellido;
+            Console.WriteLine(stNombre);
+
+            int lon = stApellido.Length;
+            Console.WriteLine("La primera letra del apellido es: {0} y tienen {1} letras", stApellido[0], lon);
+
+            Console.WriteLine("El 2º y 3º caracter es: {0}", stApellido.Substring(1,2));
+
+            Console.WriteLine("La primera letra es: {0}", stNombre[0]);
+
+            Console.WriteLine("{0}", stApellido.ToUpper().Replace("Z","S"));
+
+            Console.WriteLine("");
+
+            int itPosicion = stApellido.IndexOf("p");
+
+            if((itPosicion == -1))
+            {
+                Console.WriteLine("No existe el caracter p");
+            }
+            else
+            {
+                Console.WriteLine("La posición que ocupa el caracter p es: {0}", itPosicion);
+
+            }
+            
+            /*
+             *stApellido.Substring(0,3)
+             * stApellido.Length
+             * stApellido.ToUpper()
+             * stApellido.ToLower()
+             * .Insert(4, ", tio")
+             * .Remove(0, 6)
+             * .Replace("z", "s")
+             * 
+             * 
+             * 
+             * 
+             */
+            // Que tenga una arroba
+            // que tenga un punto
+            // 
+            bool incorrecto = true;
+            bool comp1 = true;
+            bool comp2 = true;
+            bool comp3 = true;
+            bool comp4 = true;
+
+            while (incorrecto) {
+
+                Console.WriteLine("Introduzca un email");
+                string email = Console.ReadLine();
+
+                if (email.Contains("@"))
+                {
+                    string[] emailArray = email.Split('@');
+                    if (emailArray.Length == 2)
+                    {
+                        if (!emailArray[1].Contains("."))
+                        {
+                            comp1 = false;
+                            Console.WriteLine("Falta el dominio del email");
+                        } 
+                        if (!(emailArray[1].IndexOf(".") != 0 && emailArray[1].LastIndexOf(".") != emailArray[1].Length)) {
+                            comp2 = false;
+                        } 
+
+                    } else
+                    {
+                        comp3 = false;
+                        Console.WriteLine("El correo es incorrecto, solo puede tener una @");
+                    }
+                } else
+                {
+                    comp4 = false;
+                    Console.WriteLine("El correo es incorrecto, no contiene ninguna @");
+                }
+
+                incorrecto = comp1 && comp2 && comp3 && comp4;
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Ejer5.1/Ejer5.1/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer5
+{
+    class Program
+    {
+        // Variables globales
+        struct tipoPersona
+        {
+            public string nombre;
+            public char inicial;
+            public int edad;
+            public float nota;
+        }
+
+        static void Main(string[] args)
+        {
+
+            //int[] numeros = new int[5];
+            //int suma = 0;
+            /*
+            numeros[0] = 150;
+            numeros[1] = 350;
+            numeros[2] = -50;
+            numeros[3] = 250;
+            numeros[4] = 950;
+            */
+
+            //suma = numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4];
+
+
+
+            /*
+            Console.WriteLine("Los elementos a sumar son: ");
+
+            for (int i = 0; i <= 4; i++) {
+                Console.WriteLine("Introduce un numero para la posicion: {0}", i);
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+                suma += numeros[i];
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("La suma es: {0}", suma);
+
+             
+
+
+            // Matrices
+
+            int[,] notas1 = new int[2,2];
+
+            notas1[0, 0] = 1;
+            notas1[0, 1] = 2;
+            notas1[1, 0] = 3;
+            notas1[1, 1] = 4;
+
+            int[,] notas2 = {
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                {10, 2, 3, 1, 2, 3, 4, 5, 10, 10 }
+            };
+
+            float media1 = 0;
+            float media2 = 0;
+
+            foreach (int n in notas1) {
+                
+                media1 += n;
+            }
+            media1 = media1 / notas1.Length;
+
+            foreach (int n in notas2) {
+                
+                media2 += n;
+            }
+            media2 = media2 / notas2.Length;
+
+            Console.WriteLine("La media de las notas1 es: {0}", media1);
+            Console.WriteLine("La media de las notas2 es: {0}", media2);
+            media2 = 0;
+            for (int i = 0; i<2; i++) {
+                for (int j = 0; j<10; j++) {
+                    media2 += notas2[i, j];
+                }
+            }
+            media2 = media2 / notas2.Length;
+            Console.WriteLine("La media de las notas2 es: {0}", media2);
+
+
+            */
+
+
+
+
+
+            // Variables globales
+
+            tipoPersona persona = new tipoPersona();
+            
+            persona.inicial = 'P';
+            
+            
+
+            Console.WriteLine("Creando una nueva persona, introduce nombre: ");
+            persona.nombre = Console.ReadLine();
+
+            Console.WriteLine("Edad:");
+            persona.edad = Convert.ToInt16(Console.ReadLine());
+
+
+            Console.WriteLine("Nota: ");
+            persona.nota = Convert.ToInt16(Console.ReadLine());
+
+            persona.inicial = Convert.ToChar(persona.nombre.Substring(0, 1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
